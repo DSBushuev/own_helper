d6bfc40c18f2818fdd74bcc2b4d3302ef8b7ac74
DS <deniwe1985@gmail.com>
2025-07-27 15:51:43 +0500

    8

--- app/generators.py
+++ app/generators.py
@@ -1,12 +1,12 @@
 import os
-from openai import AsyncOpenAI
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = AsyncOpenAI(
+client = OpenAI(
     api_key=os.getenv("OPENROUTER_API_KEY"),  # Ключ OpenRouter
-    api_base="https://openrouter.ai/api/v1",  # Новый базовый адрес
+    base_url="https://openrouter.ai/api/v1",  # Новый базовый адрес
 )
 
 async def gpt(question):
