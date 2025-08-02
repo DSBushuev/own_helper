088e1f04ff38a3ec4b9559ad3230fba4538f94af
DS <deniwe1985@gmail.com>
2025-08-02 13:52:12 +0500

    1.4

--- app/generators.py
+++ app/generators.py
@@ -8,8 +8,10 @@ client = AsyncOpenAI(api_key=os.getenv('AI_TOKEN'))
 
 async def gpt(question):
     response = await client.chat.completions.create(
+        extra_body={},
+        model="z-ai/glm-4.5-air:free",
         messages=[{"role": "user",
-                   "content": str(question)}],
-        model="gpt-4o"
+                   "content": str(question)}]
+        
     )
     return response
